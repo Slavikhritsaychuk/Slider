5797c5735b6eefc009033eaf4af2f6acfa49dd8b
Slavik <slavikhritsaychuk@gmail.com>
2021-02-02 22:43:16 +0300

    Pizza day2

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function openMenu() {
+    document.querySelector('.header__nav').classList.toggle('active');
+}
+
+// (function () {
+//     const burger = document.querySelector('.header__burger');
+//     burger.addEventListener('click', () => {
+//         burger.classList.toggle('active');
+//     });
+// }());
