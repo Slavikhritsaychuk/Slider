5936bb648474c2334c834c1e3557ddca29841873
Slavik <slavikhritsaychuk@gmail.com>
2021-02-03 18:32:52 +0300

    Pizza day2

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
-function openMenu() {
-    document.querySelector('.header__nav').classList.toggle('active');
-}
+let menuBurger = document.querySelector('.header__burger');
+let menuBody = document.querySelector('.header__nav');
 
-// (function () {
-//     const burger = document.querySelector('.header__burger');
-//     burger.addEventListener('click', () => {
-//         burger.classList.toggle('active');
-//     });
-// }());
+menuBurger.addEventListener('click', function (e) {
+    menuBurger.classList.toggle('active');
+    menuBody.classList.toggle('active');
+});
+
+let menuPizza = document.querySelector('.pizza__menu');
+let menuButtons = document.querySelector('.menu__buttons');
+
+menuPizza.addEventListener('click', function (e) {
+    menuPizza.classList.toggle('menuOpen');
+    menuButtons.classList.toggle('menuOpen');
+});
